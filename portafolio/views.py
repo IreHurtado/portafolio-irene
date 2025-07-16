24c5e7d1c9745b1f1a0d18db236ce4e36f56e59f
Irene Hurtado <irenehurtadoguerrero@gmail.com>
2025-07-16 18:01:19 +0200

    Agrega configuración de urls y views

--- portafolio/views.py
+++ portafolio/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'portafolio/index.html')
+
+def about(request):
+    return render(request, 'portafolio/about.html')
+
+def projects(request):
+    return render(request, 'portafolio/projects.html')
+
+def contact(request):
+    return render(request, 'portafolio/contact.html')
